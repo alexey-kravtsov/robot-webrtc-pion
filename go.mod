9362fb100e769769af564aff74e3243ca66a98b4
Alexey Kravtsov <Alexey.Kravtsov@infobip.com>
2020-03-04 18:12:12 +0300

    added example of websocket connection

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/alexey-kravtsov/robot-webrtc-pion
 go 1.13
 
 require (
-	github.com/pion/example-webrtc-applications v0.0.0-20191213210442-ba80e6759dfd
+	github.com/gorilla/websocket v1.4.1
+	github.com/pion/example-webrtc-applications v0.0.0-20191213210442-ba80e6759dfd // indirect
 	github.com/pion/webrtc/v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"github.com/alexey-kravtsov/robot-webrtc-pion/internal/webrtcservice"
+	//"github.com/alexey-kravtsov/robot-webrtc-pion/internal/webrtcservice"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
-	webrtcservice.Start()
+	//webrtcservice.Start()
+	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/signaling/robot", nil)
+	if err != nil {
+		return
+	}
+	defer c.Close()
+
+	c.WriteMessage(websocket.TextMessage, []byte("Hello"))
 
 	// Block forever
 	select {}
