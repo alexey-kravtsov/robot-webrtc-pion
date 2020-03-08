51ed9e693b6e943babc8dff9d6b73d05813304b1
Alexey Kravtsov <Alexey.Kravtsov@infobip.com>
2020-03-08 20:18:05 +0300

    added video transmission

---
+++ internal/service/message.go
@@ -0,0 +1,6 @@
+package service
+
+type Message struct {
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
---
+++ internal/service/signaling.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+func StartSignaling(schan <-chan Message, wchan chan<- Message) {
+	c, _, err := websocket.DefaultDialer.Dial("ws://192.168.0.147:8080/signaling/robot", nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer c.Close()
+
+	go sendIncoming(schan, c)
+
+	for {
+		var m Message
+		err := c.ReadJSON(&m)
+		if err != nil {
+			log.Printf("Unable to deserialize message %s \n", err)
+			continue
+		}
+
+		switch m.Type {
+		case "sdp", "ice":
+			wchan <- m
+		default:
+			log.Printf("Unknown message type: %s \n", m.Type)
+		}
+	}
+}
+
+func sendIncoming(schan <-chan Message, c *websocket.Conn) {
+	for {
+		message := <-schan
+		err := c.WriteJSON(message)
+		if err != nil {
+			log.Printf("Error sending signaling message %s \n", err)
+		}
+	}
+}
