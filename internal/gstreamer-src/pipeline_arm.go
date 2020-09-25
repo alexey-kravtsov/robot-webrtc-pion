33d54842aa62020a62bd80327e2df780d2ea4dd6
Alexey Kravtsov <Alexey.Kravtsov@infobip.com>
2020-09-25 23:08:13 +0300

    added queue in pipeline

--- internal/gstreamer-src/pipeline_arm.go
+++ internal/gstreamer-src/pipeline_arm.go
@@ -9,9 +9,12 @@ func getPipeline() string {
 
 	return `v4l2src 
 	! video/x-raw, width=640, height=480 
+	! queue 
 	! videoconvert 
 	! video/x-raw,format=I420 
+	! queue
 	! omxh264enc control-rate=1 target-bitrate=600000 
+	! queue
 	! h264parse config-interval=3 
 	! video/x-h264,stream-format=byte-stream 
 	! appsink name=appsink`
